91d5e6c7d182b2f359c9defd3bdb3dcc04568f41
petamoriken <moriken@kimamass.com>
2016-12-02 18:20:46 +0900

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# EJS + VueJS test
+
+1. `yarn`
+2. `yarn run ejs`
+
+-> create `build/` directory and files!
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+const gulp = require("gulp");
+const $ = require("gulp-load-plugins")();
+
+gulp.task("ejs", ["ejs_ja", "ejs_en"]);
+
+gulp.task("ejs_ja", function() {
+    return gulp.src("src/pages/index/**.ejs")
+        .pipe($.foreach((stream, file) => {
+            const base = file.base;
+            const data = require( path.join(base, "ja.json") );
+
+            // add lang field
+            data.lang = "ja";
+
+            return stream.pipe($.ejs(data, {"ext": ".html"}))
+        })).pipe(gulp.dest("build/"))
+});
+
+gulp.task("ejs_en", function() {
+    return gulp.src("src/pages/index/**.ejs")
+        .pipe($.foreach((stream, file) => {
+            const base = file.base;
+            const data = require( path.join(base, "en.json") );
+
+            // add lang field
+            data.lang = "en";
+
+            return stream.pipe($.ejs(data, {"ext": ".html"}))
+        })).pipe(gulp.dest("build/en/"));
+});
